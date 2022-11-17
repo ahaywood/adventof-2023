d5396e0c93e4febf88ee1e67819518b77d00a0ed
Amy Dutton <amy@ahhacreative.com>
2022-11-17 07:49:24 +0000

    🔥 Removed Powered by and Sponsored by

--- adventofjs/src/routes/Hero.svelte
+++ adventofjs/src/routes/Hero.svelte
@@ -24,28 +24,6 @@
 	<img src="/images/bells.png" alt="Christmas Bells" class="mx-auto mb-6" />
 
 	<div class="hero-title">
-		<!-- SPONSORED BY: VERCEL -->
-		<div class="sponsored flex items-center gap-x-5 justify-center">
-			<div>
-				<img
-					src="/images/heart.png"
-					srcset="/images/heart@2x.png 2x, /images/heart.png 1x"
-					alt="Cross Stitched Heart"
-				/>
-			</div>
-			<a href="http://vercel.com" target="_blank" class="text-center">
-				<h3 class="uppercase font-slabSerif text-xs sm:text-sm mb-2">Sponsored by</h3>
-				<img src="/images/logo__vercel.svg" alt="Vercel" />
-			</a>
-			<div>
-				<img
-					src="/images/heart.png"
-					srcset="/images/heart@2x.png 2x, /images/heart.png 1x"
-					alt="Cross Stitched Heart"
-				/>
-			</div>
-		</div>
-
 		<!-- TITLE -->
 		<div class="title">
 			<h2 class="font-script text-2xl lg:text-4xl mb-10">
@@ -59,28 +37,6 @@
 				<div class="text-4xl lg:text-[5rem] leading-none">JavaScript</div>
 			</h1>
 			<h2 class="font-script text-2xl lg:text-4xl">24 Challenges for the Season</h2>
-		</div>
-
-		<!-- POWERED BY COMPRESSED.FM -->
-		<div class="poweredBy flex items-center gap-x-5 justify-center">
-			<div>
-				<img
-					src="/images/heart.png"
-					srcset="/images/heart@2x.png 2x, /images/heart.png 1x"
-					alt="Cross Stitched Heart"
-				/>
-			</div>
-			<a href="http://compressed.fm" target="_blank" class="text-center">
-				<h3 class="uppercase font-slabSerif text-xs sm:text-sm mb-2">Powered by</h3>
-				<img src="/images/logo__compressed.svg" alt="Compressed.fm" />
-			</a>
-			<div>
-				<img
-					src="/images/heart.png"
-					srcset="/images/heart@2x.png 2x, /images/heart.png 1x"
-					alt="Cross Stitched Heart"
-				/>
-			</div>
 		</div>
 	</div>
 
