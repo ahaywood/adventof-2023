e42b64858f2b5efd8ea9f42daabc2f9f094f0605
Amy Haywood Dutton <me@amyhaywood.com>
2023-11-02 07:56:08 -0500

    ✨ Added OG Tags

--- adventofcss/src/routes/+layout.svelte
+++ adventofcss/src/routes/+layout.svelte
@@ -2,4 +2,21 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Advent of CSS 2023</title>
+	<link rel="manifest" href="/manifest.json" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@selfteachme" />
+	<meta name="twitter:creator" content="@selfteachme" />
+	<meta property="og:url" content="https://www.adventofcss.com/" />
+	<meta property="og:title" content="Advent of CSS" />
+	<meta property="og:description" content="24 CSS Challenges for the Season" />
+	<meta property="og:image" content="/images/og-image.png" />
+	<meta property="og:image:alt" content="Advent of CSS" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:site_name" content="Advent of CSS" />
+	<link rel="canonical" href="https://www.adventofcss.com/" />
+</svelte:head>
+
 <slot />
--- adventofjs/src/routes/+layout.svelte
+++ adventofjs/src/routes/+layout.svelte
@@ -2,4 +2,21 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Advent of JS 2023</title>
+	<link rel="manifest" href="/manifest.json" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@selfteachme" />
+	<meta name="twitter:creator" content="@selfteachme" />
+	<meta property="og:url" content="https://www.adventofjs.com/" />
+	<meta property="og:title" content="Advent of JS" />
+	<meta property="og:description" content="24 JS Challenges for the Season" />
+	<meta property="og:image" content="/images/og-image.png" />
+	<meta property="og:image:alt" content="Advent of JS" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:site_name" content="Advent of JS" />
+	<link rel="canonical" href="https://www.adventofjs.com/" />
+</svelte:head>
+
 <slot />
