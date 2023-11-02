d01965546ff62e66da19a1c4269a15edca1122e2
Amy Haywood Dutton <me@amyhaywood.com>
2023-11-02 07:58:02 -0500

    🔥 Removed friends section

--- adventofcss/src/routes/Friends.svelte
+++ adventofcss/src/routes/Friends.svelte
@@ -3,8 +3,8 @@
 	// export let friends: Friend[];
 </script>
 
-<div class="text-center font-script text-4xl mb-2">This year, I asked my friends to help.</div>
-<p class="text-center">Each day you’ll see someone new</p>
+<!-- <div class="text-center font-script text-4xl mb-2">This year, I asked my friends to help.</div>
+<p class="text-center">Each day you’ll see someone new</p> -->
 
 <!-- friends -->
 <!-- <div class="mx-2 grid grid-cols-4 gap-x-4 gap-y-4 pb-10">
@@ -14,7 +14,7 @@
 </div> -->
 
 <!-- border -->
-<div class="bg-xes h-[10px] bg-repeat-x mt-20" />
+<div class="bg-xes h-[10px] bg-repeat-x" />
 <div class="bg-diamond h-[23px] bg-repeat-x my-1" />
 <div class="bg-xes h-[10px] bg-repeat-x" />
 
