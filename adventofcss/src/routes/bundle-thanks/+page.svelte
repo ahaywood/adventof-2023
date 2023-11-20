4dad9fe9c240e2ba80f46e20ff31636bf6809804
Amy Haywood Dutton <me@amyhaywood.com>
2023-11-20 04:31:35 -0600

    ✨ Added a bundle thank you page

---
+++ adventofcss/src/routes/bundle-thanks/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	import kwesforms from 'kwesforms';
+	import Button from '$lib/components/Button.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		kwesforms.init();
+	});
+</script>
+
+<div class="flex flex-col justify-between min-h-screen">
+	<div class="fat-border" />
+
+	<a
+		class="bg-black text-white uppercase font-slabSerif text-sm py-2 px-6 absolute right-0 top-0 hover:bg-white hover:text-black"
+		href="/">[X] Close</a
+	>
+
+	<div class="layout-grid">
+		<div class="col-span-8 col-start-3 mt-24">
+			<h1 class="font-decorative uppercase text-5xl mb-5 text-center">Welcome!</h1>
+
+			<p class="text-center mb-12">
+				You've successfully joined the Advent of CSS and JavaScript.<br />You'll receive daily
+				challenges emails from me with Advent challenges,<br /> of course, you can change your mind and
+				mute or unsubscribe at any time.
+			</p>
+		</div>
+	</div>
+
+	<div>
+		<div class="zigZagBorder" />
+		<div class="snowflake-border" />
+		<div class="zigZagBorder" />
+	</div>
+</div>
+
+<style lang="postcss">
+	.fat-border {
+		@apply bg-fatBorder bg-repeat-x bg-center min-h-[180px] w-full;
+	}
+
+	.presents {
+		@apply flex items-center gap-x-4 xl:gap-x-12;
+	}
+</style>
