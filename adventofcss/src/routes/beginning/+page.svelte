a19d3529425ccfacb281dc4b29ef757086ea216d
Amy Haywood Dutton <me@amyhaywood.com>
2023-12-06 17:20:45 -0600

    ✨ Created trigger pages for sending someone to the beginning

---
+++ adventofcss/src/routes/beginning/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { onMount } from 'svelte';
+</script>
+
+<div class="flex flex-col justify-between min-h-screen">
+	<div class="fat-border" />
+
+	<a
+		class="bg-black text-white uppercase font-slabSerif text-sm py-2 px-6 absolute right-0 top-0 hover:bg-white hover:text-black"
+		href="/">[X] Close</a
+	>
+
+	<div class="layout-grid">
+		<div class="col-span-8 col-start-3 mt-24">
+			<h1 class="font-decorative uppercase text-5xl mb-5 text-center">Start from the Beginning</h1>
+
+			<p class="text-center mb-12">
+				Excellent choice! Today is your Day 1. Take as much time as you need on each challenge.
+				You'll have lifetime access to all the content through <a
+					href="https://store.selfteach.me/login"
+					target="_blank"
+					rel="noreferrer">Podia</a
+				>.
+			</p>
+			<p class="text-center mb-12">
+				Within a few minutes, you should receive the first challenge in your inbox from
+				<a href="mailto:amy@adventoffrontend.com">amy[at]adventoffrontend.com</a>.
+			</p>
+			<p class="text-center mb-12">
+				If you have any trouble, <a href="mailto:amy@adventoffrontend.com"
+					>don't hesitate to reach out directly.</a
+				>
+			</p>
+		</div>
+	</div>
+	<div>
+		<div class="zigZagBorder" />
+		<div class="snowflake-border" />
+		<div class="zigZagBorder" />
+	</div>
+</div>
+
+<style lang="postcss">
+	.fat-border {
+		@apply bg-fatBorder bg-repeat-x bg-center min-h-[180px] w-full;
+	}
+
+	.presents {
+		@apply flex items-center gap-x-4 xl:gap-x-12;
+	}
+</style>
